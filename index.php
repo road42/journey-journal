f89659aaadb15cf8d51f6a90dd5c26618c5e2c6c
Christoph Jahn <cj@road42.de>
2025-06-27 14:18:30 +0200

    Added file-blueprints

--- index.php
+++ index.php
@@ -1,5 +1,17 @@
 <?php
 
+namespace road42\JourneyJournal;
+
+use Kirby\Cms\App as Kirby;
+use Kirby\Filesystem\F as F;
+
+@include_once __DIR__ . '/vendor/autoload.php';
+
 Kirby::plugin('road42/journey-journal', [
-	// plugin magic happens here
+	'blueprints' => [
+        'files/journey-journal-image' => __DIR__ . '/blueprints/files/journey-journal-image.yml',
+        'files/journey-cover' => __DIR__ . '/blueprints/files/journey-cover.yml',
+        'files/journey-gallery-image' => __DIR__ . '/blueprints/files/journey-gallery-image.yml',
+        'files/journey-gps-file' => __DIR__ . '/blueprints/files/journey-gps-file.yml'
+    ]
 ]);

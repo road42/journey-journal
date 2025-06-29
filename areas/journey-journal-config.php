011f383fa35f2eb90bc49acad0478f7f155e14fa
Christoph Jahn <cj@road42.de>
2025-06-29 14:12:37 +0200

    Config panel area is only shown for admins

--- areas/journey-journal-config.php
+++ areas/journey-journal-config.php
@@ -1,24 +1,30 @@
 <?php
 
+
 return function ($kirby) {
-    return [
-        'label' => t('journey-journal.config.label'),
-        'icon'  => 'car',
-        'menu'  => true,
-        'link'  => 'journey-journal-config',
-        'views' => [
-            [
-                'pattern' => 'journey-journal-config',
-                'action'  => function () {
-                    return [
-                        'component' => 'journey-journal-place-icons',
-                        'props' => [
-                            'headline' => t('journey-journal.config.headline'),
-                        ]
-                    ];
-                }
+    if (kirby()->user() && kirby()->user()->isAdmin())
+    {
+        return [
+            'label' => t('journey-journal.config.label'),
+            'icon'  => 'car',
+            'menu'  => true,
+            'link'  => 'journey-journal-config',
+            'views' => [
+                [
+                    'pattern' => 'journey-journal-config',
+                    'action'  => function () {
+                        return [
+                            'component' => 'journey-journal-place-icons',
+                            'props' => [
+                                'headline' => t('journey-journal.config.headline'),
+                            ]
+                        ];
+                    }
+                ]
             ]
-        ]
-    ];
+        ];
+    }
+
+    return [];
 }
 ?>
